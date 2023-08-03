d87dec3bba2c0db6ccc13c85a05b452b74d7f62f
arshildudhat <arshildudhat261104@gmail.com>
2023-08-03 18:35:27 +0530

    done

--- src/View/index.js
+++ src/View/index.js
@@ -1,9 +1,44 @@
 import React from "react";
+import Muibutton from "./Button/Button";
+import Grid from "./grid/Grid";
 
 function View() {
-    return(
-        <>View Component</>
-    )
+  const logMessage = () => {
+    console.log("Hello from logMessage function!");
+  };
+  return (
+    <>
+      <Muibutton
+        variant={""}
+        bgcolor={""}
+        loading={""}
+        size={""}
+        startIcon={""}
+        endIcon={""}
+        text={""}
+        onClick={null}
+        yz={{
+          width: "",
+          height: "",
+          fontSize: "",
+          borderRadius: "",
+          fontWeight: "",
+          fontStyle: "",
+          textTransform: "",
+          color: "",
+          shadow: "",
+          hoverBgColor: "",
+          hoverColor: "",
+          hoverShadow: "",
+          transition: "",
+          lodingcolor: "",
+          lodingwidth: "",
+          lodingheight: "",
+        }}
+      />
+      <Grid />
+    </>
+  );
 }
 
 export default View;
