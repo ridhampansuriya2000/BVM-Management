8a0b640998f7419bf0508645a04dae3ef0dbe3e1
yagnik <yagnikdevani00@gmail.com>
2023-08-03 09:21:41 +0530

    add mui package and made  select button

--- src/View/index.js
+++ src/View/index.js
@@ -1,11 +1,35 @@
 import React from "react";
 import Selection from "./Selection/Selection";
 function View() {
-   
   return (
     <>
       <h1>View Component</h1>
-      <Selection value="10" />
+      <Selection
+        // variant : [outlined,filled,standard]
+        // variant="standard"
+        multiple
+        label="Ageg"
+        labelName="hari"
+        allselectsx ={{margin:"0px 20px"}}
+        selectsx={{ width: "200px" }}
+        optionsx={{ width: "200px" }}
+        options={[
+          { value: "", Text: "None" },
+          { value: "Age1", Text: "Age1" },
+          { value: "Age2", Text: "Age2" },
+        ]}
+      />
+
+      <Selection
+       variant="standard"
+       label="Ageg"
+       labelName="hari"
+        options={[
+          { value: "", Text: "None" },
+          { value: "Age1", Text: "Age1" },
+          { value: "Age2", Text: "Age2" },
+        ]}
+      />
     </>
   );
 }
