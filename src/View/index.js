02bb3b7448e95e4d7c1fcadc1bb33c9332797aed
ronakkoladiya <ronakkoladiya0010@gmail.com>
2023-08-03 17:55:14 +0530

    Responsive

--- src/View/index.js
+++ src/View/index.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import TextFields from "./TextFields/TextFields";
 
-import {Grid} from '@mui/material';
+import {Grid, Container} from '@mui/material';
 
 function View(){
 
@@ -18,12 +18,22 @@ function View(){
 
     return(
         <>
-            <Grid container>
-                <Grid xs={3}>
-                    <span>Email*</span><br/>
-                    <TextFields type={''} label={''} variant={''} placeholder={''} width={''} autocomplete={''} value={data} validateData={validateData} />
+            <Container sx={{mt: 5}}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <span>Email*</span><br/>
+                        <TextFields type={''} label={''} variant={''} placeholder={''} width={''} autocomplete={''} value={data} validateData={validateData} />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <span>Email*</span><br/>
+                        <TextFields type={''} label={''} variant={''} placeholder={''} width={''} autocomplete={''} value={data} validateData={validateData} />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <span>Email*</span><br/>
+                        <TextFields type={''} label={''} variant={''} placeholder={''} width={''} autocomplete={''} value={data} validateData={validateData} />
+                    </Grid>
                 </Grid>
-            </Grid>
+            </Container>
         </>
     );
 }
