541f9582ba2df2b7638428adb6dd553e510dacc5
yagnik <yagnikdevani00@gmail.com>
2023-08-03 18:10:01 +0530

    add many selection fro example

--- src/View/index.js
+++ src/View/index.js
@@ -7,12 +7,12 @@ function View() {
       <Selection
         // variant : [outlined,filled,standard]
         // variant="standard"
-        
+
         native
         multiple
         labelName="ram"
         allselectsx={{ margin: "0px 20px" }}
-        selectsx={{ width: "200px",dropdowniconcolor:"green" }}
+        selectsx={{ width: "200px", dropdowniconcolor: "green" }}
         optionsx={{ width: "200px" }}
         options={[
           { value: "", Text: "None" },
@@ -22,28 +22,28 @@ function View() {
       />
 
       <Selection
-              sx= {{width:"200px"}}
+        sx={{ width: "200px" }}
         options={[
           { value: "", Text: "None" },
           { value: "Age1", Text: "Age1" },
           { value: "Age2", Text: "Age2" },
         ]}
       />
-   
 
-      <Selection
-
-        variant="filled"
-        labelName="zsd"
-        allselectsx={{ margin: "0px 20px" }}
-        selectsx={{ width: "200px" }}
-        optionsx={{ width: "200px",backgroundColor: "red" }}
-        options={[
-          { value: "", Text: "None" },
-          { value: "Age1", Text: "Age1" },
-          { value: "Age2", Text: "Age2" },
-        ]}
-      />
+      <div style={{ margin: "0px 20px" }}>
+        <Selection
+          variant="filled"
+          labelName="zsd"
+          // allselectsx={{ margin: "0px 20px" }}
+          selectsx={{ width: "200px" }}
+          optionsx={{ width: "200px", backgroundColor: "red" }}
+          options={[
+            { value: "", Text: "None" },
+            { value: "Age1", Text: "Age1" },
+            { value: "Age2", Text: "Age2" },
+          ]}
+        />
+      </div>
 
       <Selection
         variant="standard"
@@ -57,19 +57,40 @@ function View() {
           { value: "Age2", Text: "Age2" },
         ]}
       />
+      <br />
 
-
-<Selection
-sx={{boxShadow: "0"}}
-allselectsx={{borderRadius:"500px",outline:"none"}}
-      selectsx = {{backgroundColor:"#F2F5FF" ,dropdowniconcolor:"black" ,color:"#808297",boxShadow:"none !important" ,borderRadius:"500px",border:"none"}}
-      optionsx = {{border:"red",dropdowniconcolor:"black" ,color:"#808297",boxShadow:"none !important" ,border:"none"}}
-      options={[
-        { value: "", Text: "None" },
-        { value: "Age1", Text: "Age1" },
-        { value: "Age2", Text: "Age2" },
-      ]}
+      <Selection
+        sx={{ boxShadow: "0" }}
+        allselectsx={{
+          borderRadius: "500px",
+          outline: "none",
+          margin: "10px 20px",
+          width: "100px",
+        }}
+        selectsx={{
+          backgroundColor: "#F2F5FF",
+          dropdowniconcolor: "black",
+          color: "#808297",
+          boxShadow: "none !important",
+          borderRadius: "500px",
+          border: "none",
+        }}
+        optionsx={{
+          border: "red",
+          dropdowniconcolor: "black",
+          color: "#808297",
+          boxShadow: "none !important",
+          border: "none",
+        }}
+        options={[
+          { value: "", Text: "None" },
+          { value: "Age1", Text: "Age1" },
+          { value: "Age2", Text: "Age2" },
+        ]}
       />
+
+
+      
     </>
   );
 }
